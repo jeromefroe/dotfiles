fbd0f110f439e497b6eb8dc60eca04267ec6c53a
Jerome Froelich <jeromefroe@uber.com>
2018-03-12 23:45:57 -0400

    Update configure script

--- configure.sh
+++ configure.sh
@@ -4,7 +4,8 @@ for file in .{path,bash_prompt,exports,aliases,functions,extra,vim,vimrc}; do
     source_file="$HOME/dev/dotfiles/$file"
     target_file="$HOME/$file"
     
-    # Ensure that we have read permissions for the file.
-    [ -r "$source_file" ] && ln -s $source_file $target_file ;
+    # Ensure that we have read permissions for the file and the target file does
+    # not already exist.
+    [ -r "$source_file" ] && [ ! -e "$target_file" ] && ln -s $source_file $target_file ;
 done;
 
