1f270282392217385a14fc48fe3718ff99257fe4
Jerome Froelich <jeromefroe@uber.com>
2018-05-20 08:12:41 -0400

    Color prompt based on exit code of previous command

--- configure.sh
+++ configure.sh
@@ -5,7 +5,8 @@ for file in .{path,bash_prompt,exports,aliases,functions,extra,vim,vimrc}; do
     target_file="$HOME/$file"
     
     # Ensure that we have read permissions for the file and the target file does
-    # not already exist.
+    # not already exist. Order is important for one case, .extra must come after
+    # .bash_prompt since it needs to modify the prompt command.
     [ -r "$source_file" ] && [ ! -e "$target_file" ] && ln -s $source_file $target_file ;
 done;
 
